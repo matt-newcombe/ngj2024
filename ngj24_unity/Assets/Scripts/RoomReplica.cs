da3d25f4cb5aa8ddc6ebbcef123c620f796e85b5
Martin <mfasterholdt@gmail.com>
2024-04-20 13:50:39 +0200

    small tweaks to room synchronization

--- ngj24_unity/Assets/Scripts/RoomReplica.cs
+++ ngj24_unity/Assets/Scripts/RoomReplica.cs
@@ -2,18 +2,29 @@ using UnityEngine;
 
 public class RoomReplica : MonoBehaviour
 {
-    public bool attached;
+    public bool attached;    
+    public int placementScale = 20;
 
     [HideInInspector, SerializeField]
     public Room room;
-    
-    public int placementScale = 20;
+
+    private void Update()
+    {
+        //TODO replace this, only update when replica has been moved or attached/detached
+        UpdateRoom();
+    }
 
     private void OnDrawGizmos()
     {
+        if (!Application.isPlaying)
+            UpdateRoom();
+    }
+
+    public void UpdateRoom()
+    {
         if (!room)
             return;
-       
+
         room.gameObject.SetActive(attached);
 
         if (!attached || !GameManager.Instance)
@@ -21,11 +32,9 @@ public class RoomReplica : MonoBehaviour
 
         Room centerRoom = GameManager.Instance.centerRoom;
         Transform pivot = GameManager.Instance.centerPivot;
-        
+
         if (!centerRoom || !pivot || room == centerRoom)
             return;
-
-        Vector3 offset =  centerRoom.transform.position + pivot.position;
 
         Vector3 localPos = pivot.InverseTransformPoint(transform.position);
         Vector3 worldPos = pivot.TransformPoint(localPos * placementScale - pivot.localPosition);
